8ee67365a46e1043f98e69eafb49de64a422b332
lichunlei10086 <522216713@qq.com>
2018-06-29 10:39:24 +0800

    add all

---
+++ dao/src/main/java/com/luyh/projectv1/dao/config/MyBatisConfig.java
@@ -0,0 +1,35 @@
+package com.luyh.projectv1.dao.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+/**
+ * Created by trons on 16-4-17.
+ */
+@Configuration
+public class MyBatisConfig {
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource(){
+    	
+        return new org.apache.tomcat.jdbc.pool.DataSource();
+    }
+
+    @Bean(name = "sqlSessionFactory")
+    public SqlSessionFactory sqlSessionFactoryBean() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        sqlSessionFactoryBean.setDataSource(dataSource());
+
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+
+        sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:/mybatis/*.xml"));
+
+        return sqlSessionFactoryBean.getObject();
+    }
+}
